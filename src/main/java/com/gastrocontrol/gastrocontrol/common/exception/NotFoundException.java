2a21926495072d38a1476ec234351cb4f34c9ab5
Jonathan Ray Hendrix López <jrhendrixdev@gmail.com>
2025-11-25 22:21:42 +0100

    Initial commit

---
+++ src/main/java/com/gastrocontrol/gastrocontrol/common/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.gastrocontrol.gastrocontrol.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends ApiException {
+
+    public NotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND);
+    }
+}
